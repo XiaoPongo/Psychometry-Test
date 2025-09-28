6ab03e9cddea56d020e4f2e872c0800cf14f91c5
XiaoPongo <100044523+XiaoPongo@users.noreply.github.com>
2025-09-28 22:47:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/Psychometry-Test/'   // <-- important for GitHub Pages
+  base: '/'       // for Vercel (GitHub Pages needed /Psychometry-Test/)
 })
