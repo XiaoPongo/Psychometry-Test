82f7dcefb9839120eb47e5694c912ee5536ef922
XiaoPongo <100044523+XiaoPongo@users.noreply.github.com>
2025-09-28 22:20:46 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  plugins: [svelte()],
+  base: 'Psychometry-Test', // <- IMPORTANT
+});
